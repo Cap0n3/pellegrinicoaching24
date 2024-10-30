46b82c46203498eda95b3176a02561149f3486d9
Cap0n3 <cap0n3@protonmail.com>
2024-10-30 19:57:04 +0100

    Started to add wave dividers

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,6 +32,14 @@ export default function Hero() {
                     </Button>
                 </Slide>
             </div>
+            <div className="absolute bottom-0 left-0 w-full">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                    <g transform="matrix(1,0,0,0.362399,0,204.032)">
+                        <path className="fill-current text-[#FFF9EB] dark:text-gray-900" d="M0,288L48,272C96,256 192,224 288,197.3C384,171 480,149 576,165.3C672,181 768,235 864,250.7C960,267 1056,245 1152,250.7C1248,256 1344,288 1392,304L1440,320L0,320L0,288Z">
+                        </path>
+                    </g>
+                </svg>
+            </div>
         </section>
     );
 }
---
+++ components/common/Wave.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    position: 'top' | 'bottom'
+}
+
+export default function Wave({
+    position
+}: Props) {
+  return (
+    <div>Wave</div>
+  )
+}
